8be78f6b33271b08aeb008d67a6078e7df444326
pwnker <tilen.okretic@gmail.com>
2024-04-02 12:35:58 +0200

    update: clean up code

--- main.c
+++ main.c
@@ -9,13 +9,10 @@ int main(void)
 
     for (size_t i = 0; i < BOARD_CAP-2; ++i) {
         for (size_t j = 0; j < BOARD_CAP; ++j) {
-            printf("%c", " *"[board[j]], stdout);
+            printf("%c", " *"[board[j]]);
         }
         printf("\n");
 
-        int a = board[0];
-        int b = board[1];
-        int c = board[2];
         int pattern = (board[0] << 2) | (board[1] << 1);
         for (size_t j = 1; j < BOARD_CAP-1; ++j) {
             pattern = ((pattern << 1) & 7) | board[j+1];
